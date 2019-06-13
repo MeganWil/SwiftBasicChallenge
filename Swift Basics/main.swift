ca17ecf3ad951fe8faa43455a7bceb1ec26963c8
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-06-13 10:57:08 -0500

    Adding Files

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Megan Wilson on 6/12/19.
+//  Copyright © 2019 Megan Wilson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
